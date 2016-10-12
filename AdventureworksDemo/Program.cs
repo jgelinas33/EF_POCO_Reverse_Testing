94d424892ee0441c14165afe5b4524a221fb2445
jgelinas <jgelinas3333@gmail.com>
2016-10-12 07:59:48 -0400

    Initial Commit

---
+++ AdventureworksDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureworksDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var database = new MyDbContext())
+            {
+                var currentData = database.Person_People;
+                foreach (var item in currentData)
+                {
+                    Console.WriteLine(item.LastName + ", " + item.FirstName);
+                }
+            }
+        }
+    }
+}
